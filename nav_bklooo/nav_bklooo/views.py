b9a18fdbc2d367c0cd64c392bab36f5f522eedc5
bklooo <bklooo@qq.com>
2021-07-16 23:37:54 +0800

    first commit

---
+++ nav_bklooo/nav_bklooo/views.py
@@ -0,0 +1,9 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# 主页
+def index(request):
+    head = {
+        'top_name': '冰阔落导航'}
+    return render(request, 'index.html', head)
