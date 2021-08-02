ed3d0cdfa8c7007909e4be350e3848ccf7aeb922
bklooo <bklooo@qq.com>
2021-08-02 16:06:06 +0800

    增加更多功能

---
+++ nav_bklooo/Navigation/more.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from Navigation.models import msg
+import urllib.parse
+
+
+def more(request):
+    request.encoding = 'utf-8'
+    if request.GET['class']:
+        r = request.GET['class']
+        data = urllib.parse.unquote(r)
+        msglist = msg.objects.all().values()
+        result = []
+        for i in msglist:
+            if data == i['type']:
+                result.append(i)
+        if len(result) != 0:
+            return render(request, "more.html", {"result": result, "classification": data})
+        else:
+            return render(request, "more.html", {"null": "抱歉，数据为空"})
+
+    else:
+        return render(request, "more.html", {"null": "参数为空"})
--- nav_bklooo/Navigation/urls.py
+++ nav_bklooo/Navigation/urls.py
@@ -1,5 +1,5 @@
 from django.urls import re_path
-from . import getUrl, search, return_web_name
+from . import getUrl, search, return_web_name, more
 
 # 所有的网页路径
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     re_path(r'search/', search.searchAll),
     re_path(r'sitename/', return_web_name.getWebName),
     re_path(r'ransite/', getUrl.get_ran_site),
+    re_path(r'more/', more.more),
 ]
