fbdd3c4acf5781b614ef7979c264dd89c7af513f
bklooo <bklooo@qq.com>
2021-08-06 23:41:46 +0800

    更新

---
+++ siteSpider/siteSpider/spiders/navbot_6.py
@@ -0,0 +1,55 @@
+import scrapy
+from siteSpider.items import SitespiderItem
+from urllib.parse import urljoin
+import datetime
+import re
+
+
+class Navbot6Spider(scrapy.Spider):
+    name = 'navbot_6'
+    start_urls = ['https://top.chinaz.com/hangye/index_wangluo_sheji.html/']
+
+    def parse(self, response):
+        item = SitespiderItem()
+        if(response.status == 200):
+            for i in response.xpath('//*[@id="content"]/div[4]/div[3]/div[2]/ul/li[position()>1]'):
+                item['name'] = i.xpath('div[2]/h3/a/text()').get()
+                item['link'] = "http://" + i.xpath('div[2]/h3/span/text()').get()
+                print(item)
+                yield scrapy.Request(item['link'], callback=self.get_msg, cb_kwargs=dict(name=item['name'], link=item['link']))
+            next_page = urljoin('https://top.chinaz.com/hangye/index_jiaotonglvyou_lvyou.html/', response.xpath('//*[@id="content"]/div[4]/div[3]/div[2]/div[2]/a[11]/@href').get())
+            yield scrapy.Request(next_page, callback=self.parse)
+
+    def get_msg(self, response, name, link):
+        item = SitespiderItem()
+        if(response.status == 200):
+            item['name'] = name
+            item['type'] = "设计素材"
+            item['link'] = link
+            icon_url = response.xpath("//link[@rel='icon']/@href | //link[@rel='shortcut icon']/@href").get()
+            if(icon_url):
+                pattern1 = re.compile(r'[.]+')
+                pattern2 = re.compile(r'[/]+')
+                pattern3 = re.compile(r'[^./(http)]+')
+                if(pattern1.match(icon_url)):
+                    item['icon'] = urljoin(response.url, icon_url)
+                elif(pattern2.match(icon_url)):
+                    if(pattern2.match(icon_url).span()[1] == 1):
+                        item['icon'] = urljoin(response.url, icon_url)
+                    else:
+                        item['icon'] = icon_url
+                elif(pattern3.match(icon_url)):
+                    item['icon'] = urljoin(response.url, icon_url)
+                else:
+                    item['icon'] = icon_url
+            else:
+                item['icon'] = ""
+            info = response.xpath("//meta[@name='description']/@content").get()
+            if(info):
+                item['info'] = info
+            else:
+                item['info'] = ""
+            item['time'] = datetime.datetime.now().strftime('%Y-%m-%d')
+            yield item
+        else:
+            print("error")
