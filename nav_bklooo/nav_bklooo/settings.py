5f0ab19b182391ab05c8aac9ad5a79fd7bd04979
bklooo <58132568+bklooo@users.noreply.github.com>
2021-08-12 09:08:27 +0800

    Update settings.py

--- nav_bklooo/nav_bklooo/settings.py
+++ nav_bklooo/nav_bklooo/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'nav.bklooo',
-        'USER': 'root',
-        'PASSWORD': 'coolboy.',
+        'USER': '账号',
+        'PASSWORD': '密码',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
